a76f392899c7b57ecb64b6f478304c3b7edaabe1
nagasho <abc@example.com>
2021-05-14 23:02:49 +0900

    完成

---
+++ app/src/main/java/com/nagase/nagasho/imageview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nagase.nagasho.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import coil.api.load
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val url = "https://life-is-tech.com/materials/images/logo-square.png"
+        val imageView = findViewById<ImageView>(R.id.imageView)
+
+        imageView.load(url)
+    }
+}
